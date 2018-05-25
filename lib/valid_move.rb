24ec50bddbb106fc7503def8cfcf6178f2a495df
Joseph Senders <jasenders@gmail.com>
2018-05-25 19:08:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
-  elsif index.to_i.between?(0, 8) 
+  elsif index.to_i.between?(0, 8)
     return true
   end
 end
